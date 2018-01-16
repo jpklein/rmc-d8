8619d1d1e03d7f9a06713dd908e2cf27047cd436
jpklein <jpklein@gmail.com>
2018-01-15 22:55:05 -0800

    Enables drush migration on pantheon - see https://kalamuna.atlassian.net/wiki/spaces/KALA/pages/116230409/Migrating+to+Drupal+8+from+previous+versions

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -23,3 +23,11 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+$settings['install_profile'] = 'rmc_base';
+
+# When on Pantheon, connect to a D7 database.
+$migrate_settings = __DIR__ . "/settings.migrate-on-pantheon.php";
+if (file_exists($migrate_settings) && isset($_ENV['PANTHEON_ENVIRONMENT'])) {
+  include $migrate_settings;
+}
+
